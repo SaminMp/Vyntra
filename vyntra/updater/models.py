c7bef1d7dfe4188b9ec9d8cc44ac7d6a7b572ad1
unknown <saminmehdypoor@gmail.com>
2026-09-13 21:59:40 +0330

    Check: checking for the updater

--- vyntra/updater/models.py
+++ vyntra/updater/models.py
@@ -6,6 +6,9 @@ from dataclasses import dataclass, field
 from typing import Dict, List, Optional
 
 
+
+
+
 @dataclass
 class ReleaseAsset:
     """Represents a downloadable binary or metadata asset attached to a GitHub Release."""
